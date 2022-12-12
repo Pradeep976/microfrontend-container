ec7bbed8cd8bde0865f84ecb27226e7e7c5215a2
Pradeep Saravanan <pradeep.saravanan@mallow-tech.com>
2022-12-12 14:10:51 +0530

    init

--- pages/catalogOnMain.js
+++ pages/catalogOnMain.js
@@ -1,5 +1,5 @@
 import dynamic from "next/dynamic";
 
-const Page = dynamic(() => import('child/pages/catalog'), { ssr: false });
+const Page = dynamic(() => import('child/pages/clients'), { ssr: false });
 
 export default Page;
